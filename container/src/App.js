57376c96f25c431a8db6eb3f93a4a36b60aa539e
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-11-18 13:31:36 -0600

    integrando marketing a container

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { mount } from 'marketing/MarketingApp';
 
 const App = () => {
+  console.log('mount: ', mount);
   return (
     <div>
       <h1>App component</h1>
