a527fb4a2d5ab52693eeb036f8e6c9b40b152930
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-11-30 19:04:07 -0600

    Manteniendo history en container

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,13 +1,20 @@
 import React, { useRef, useEffect } from 'react';
 import { mount } from 'marketing/MarketingApp';
+import { useHistory } from 'react-router-dom';
 
 const MarketingApp = () => {
   const refMarketing = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
     mount(refMarketing.current, {
-      onNavigate: () => {
-        console.log('Container navegará en marketing');
+      onNavigate: (location) => {
+        const nextPathName = location.pathname;
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathName) {
+          history.push(nextPathName);
+        }
       }
     });
   }, []);
