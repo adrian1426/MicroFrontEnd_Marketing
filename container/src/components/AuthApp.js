1d946dcc86ccc2f6d9ad95d2300734f44741349f
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-12-03 15:59:03 -0600

    integrando a container proyecto Auth

---
+++ container/src/components/AuthApp.js
@@ -0,0 +1,29 @@
+import React, { useRef, useEffect } from 'react';
+import { mount } from 'auth/AuthApp';
+import { useHistory } from 'react-router-dom';
+
+const AuthApp = () => {
+  const refAuth = useRef(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const { onParentNavigate } = mount(refAuth.current, {
+      onNavigate: (location) => {
+        const nextPathName = location.pathname;
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathName) {
+          history.push(nextPathName);
+        }
+      }
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
+
+  return (
+    <div ref={refAuth}></div>
+  );
+};
+
+export default AuthApp;
